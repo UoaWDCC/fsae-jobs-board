025d50b0361435cb3259ccad3bf32fdc4e3ab6ab
misssn1998 <siennanguyen0503@gmail.com>
2024-05-16 02:58:29 +1200

    add navbar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
+import Navbar from '@components/Navbar';
 
 export default function App() {
   return (
