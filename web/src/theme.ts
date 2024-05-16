2c697f14dfbb55c12f18d2b0613907171174be29
Jolin Chen <jche521@aucklanduni.ac.nz>
2024-05-16 14:59:16 +1200

    initialize global styling of color, font and heading

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -1,5 +1,32 @@
-import { createTheme } from '@mantine/core';
+import { MantineColorsTuple, colorsTuple, createTheme, rem } from '@mantine/core';
+
+const customAzureBlue = colorsTuple('#0091ff');
+const customDarkBlue = colorsTuple('#00467f');
+const customPapayaOrange = colorsTuple('#f78f1f');
+const customElectricOrange = colorsTuple('#ff8400');
+const customCharcoalGrey = colorsTuple('#373737');
+const customMercurySilver = colorsTuple('#a5a5a5');
 
 export const theme = createTheme({
-  /** Put your mantine theme override here */
+  colors: {
+    customAzureBlue,
+    customDarkBlue,
+    customPapayaOrange,
+    customElectricOrange,
+    customCharcoalGrey,
+    customMercurySilver,
+  },
+  fontFamily: 'Cerebri Sans, Calibri',
+
+  headings: {
+    fontFamily: 'Cerebri Sans, Calibri',
+    sizes: {
+      h1: { fontSize: rem(70) },
+      h2: { fontSize: rem(60) },
+      h3: { fontSize: rem(50) },
+      h4: { fontSize: rem(40) },
+      h5: { fontSize: rem(30) },
+      h6: { fontSize: rem(20) },
+    },
+  },
 });
