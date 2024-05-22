29c675d1815c258548088651bb1aa7033d5942ba
Kinzi <100537572+Kinzi-c@users.noreply.github.com>
2024-05-23 10:22:45 +1200

    Add dummy pages for routing tutorial

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,9 +1,8 @@
-import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Button, Flex } from '@mantine/core';
 import { NavLink } from 'react-router-dom';
 
-export function HomePage() {
+export function Login() {
   return (
     <>
       {/* Temporary buttons for route testing */}
@@ -13,13 +12,10 @@ export function HomePage() {
             Sign up
           </Button>
         </NavLink>
-        <NavLink to="/login">
-          <Button color="customAzureBlue">Log in</Button>
+        <NavLink to="/home">
+          <Button color="customAzureBlue">Home</Button>
         </NavLink>
       </Flex>
-
-      <Welcome />
-      <ColorSchemeToggle />
     </>
   );
 }
---
+++ web/src/pages/Signup.student.tsx
@@ -0,0 +1,23 @@
+import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Button, Flex } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
+
+export function SignUp() {
+    return (
+      <>
+        {/* Temporary buttons for route testing */}
+        <Flex justify="right" gap="md" mt="md" mr="md">
+          <NavLink to="/home">
+            <Button variant="filled" color="customPapayaOrange">
+              Home
+            </Button>
+          </NavLink>
+          <NavLink to="/login">
+            <Button color="customAzureBlue">Log in</Button>
+          </NavLink>
+        </Flex>
+        <ColorSchemeToggle />
+      </>
+    );
+  }
+  
