c0aa47fa342b49d06acb0a6b04486e87623bff61
Jolin <jche521@aucklanduni.ac.nz>
2024-06-29 11:53:04 +1200

    login page done

--- web/src/app/pages/Login.page.tsx
+++ web/src/app/pages/Login.page.tsx
@@ -1,10 +1,11 @@
-import { Button, Flex, Title } from '@mantine/core';
+import { Button, Flex, Image, Title } from '@mantine/core';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setUserType } from '../features/user/userSlice';
 import { toast } from 'react-toastify';
 import { LoginForm } from '../components/AuthForms/LoginForm';
 import classes from './page.module.css';
+import logo from '../assets/images/logo.png';
 
 export function Login() {
   const dispatch = useDispatch();
@@ -29,9 +30,11 @@ export function Login() {
   return (
     <div className={classes.wrapper}>
       {/* Temporary buttons for protected routes testing purpose*/}
-      <NavLink to="/" className={classes.banner}>
-        <Button color="customAzureBlue">Home</Button>
-      </NavLink>
+      <div className={classes.banner}>
+        <NavLink to="/" className={classes.logo}>
+          <Image h={25} src={logo} fit="contain" />
+        </NavLink>
+      </div>
       <LoginForm />
     </div>
   );
