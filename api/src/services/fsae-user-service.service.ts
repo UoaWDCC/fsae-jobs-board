4671d62fc4a77b35c70bb30f112626370d25332c
fsit869 <62918263+fsit869@users.noreply.github.com>
2024-07-05 18:39:46 +1200

    Added authentication library

---
+++ api/src/services/fsae-user-service.service.ts
@@ -0,0 +1,10 @@
+import {injectable, /* inject, */ BindingScope} from '@loopback/core';
+
+@injectable({scope: BindingScope.SINGLETON})
+export class FsaeUserServiceService {
+  constructor(/* Add @inject to inject parameters */) {}
+
+  /*
+   * Add service methods here
+   */
+}
---
+++ api/src/services/index.ts
@@ -0,0 +1,2 @@
+export * from './fsae-user-service.service';
+export * from './jwt-service.service';
