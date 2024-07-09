fc258c42a8ef9afd5d3595f665ca61bbec97acf4
Jolin <jche521@aucklanduni.ac.nz>
2024-07-09 21:56:09 +1200

    add sponsor and alumni form

--- web/src/app/components/AuthForms/SignupForm.tsx
+++ web/src/app/components/AuthForms/SignupForm.tsx
@@ -13,7 +13,7 @@ import { useState } from 'react';
 import classes from './authform.module.css';
 
 const SignupForm = () => {
-  const [role, setRole] = useState('student');
+  const [role, setRole] = useState('alumni');
   return (
     <div className={classes.signupFormContainer}>
       {/* logo */}
@@ -21,6 +21,7 @@ const SignupForm = () => {
         Join the FSAE:47 Job Board Community
       </Title>
 
+      {/* sponsor form */}
       {role === 'student' && (
         <form className={classes.form}>
           <Stack gap={30}>
@@ -36,6 +37,113 @@ const SignupForm = () => {
               required
               classNames={{ label: classes.formLabel }}
             />
+            <TextInput label="Email" size="lg" required classNames={{ label: classes.formLabel }} />
+            <Flex justify="space-between">
+              <PasswordInput
+                label="Password"
+                size="lg"
+                required
+                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
+              />
+              <PasswordInput
+                label="Confirmed Password"
+                size="lg"
+                required
+                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
+              />
+            </Flex>
+            <Checkbox
+              size="lg"
+              label={
+                <>
+                  I accept{' '}
+                  <Anchor href="https://mantine.dev" target="_blank" inherit>
+                    terms and conditions
+                  </Anchor>
+                </>
+              }
+              required
+            />
+            <Button color="var(--mantine-color-customAzureBlue-1)" size="lg">
+              Confirm
+            </Button>
+          </Stack>
+        </form>
+      )}
+
+      {/* student form */}
+      {role === 'sponsor' && (
+        <form className={classes.form}>
+          <Stack gap={30}>
+            <TextInput
+              label="Company"
+              size="lg"
+              required
+              classNames={{ label: classes.formLabel }}
+            />
+            <TextInput
+              label="Phone Number"
+              size="lg"
+              required
+              classNames={{ label: classes.formLabel }}
+            />
+            <TextInput label="Email" size="lg" required classNames={{ label: classes.formLabel }} />
+            <Flex justify="space-between">
+              <PasswordInput
+                label="Password"
+                size="lg"
+                required
+                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
+              />
+              <PasswordInput
+                label="Confirmed Password"
+                size="lg"
+                required
+                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
+              />
+            </Flex>
+            <Checkbox
+              size="lg"
+              label={
+                <>
+                  I accept{' '}
+                  <Anchor href="https://mantine.dev" target="_blank" inherit>
+                    terms and conditions
+                  </Anchor>
+                </>
+              }
+              required
+            />
+            <Button color="var(--mantine-color-customAzureBlue-1)" size="lg">
+              Confirm
+            </Button>
+          </Stack>
+        </form>
+      )}
+
+      {/* alumni form */}
+      {role === 'alumni' && (
+        <form className={classes.form}>
+          <Stack gap={30}>
+            <TextInput
+              label="First Name"
+              size="lg"
+              required
+              classNames={{ label: classes.formLabel }}
+            />
+            <TextInput
+              label="Last Name"
+              size="lg"
+              required
+              classNames={{ label: classes.formLabel }}
+            />
+            <TextInput
+              label="Company"
+              size="lg"
+              required
+              classNames={{ label: classes.formLabel }}
+            />
+
             <TextInput label="Email" size="lg" required classNames={{ label: classes.formLabel }} />
             <Flex justify="space-between">
               <PasswordInput
