82bf6ce72796e83a6820c64909237c8deb9b4738
Jolin <jche521@aucklanduni.ac.nz>
2024-07-10 12:13:46 +1200

    restructure signup page

--- web/src/app/components/AuthForms/SignupForm.tsx
+++ web/src/app/components/AuthForms/SignupForm.tsx
@@ -2,181 +2,116 @@ import {
   Anchor,
   Button,
   Checkbox,
-  Container,
   Flex,
   PasswordInput,
   Stack,
   TextInput,
   Title,
 } from '@mantine/core';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import classes from './authform.module.css';
 
+interface Field {
+  label: string;
+  name: string;
+}
+
+interface FormComponentProps {
+  fields: Field[];
+  role: string;
+  onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+}
+
+const fieldsByRole: { [key: string]: Field[] } = {
+  student: [
+    { label: 'First Name', name: 'firstName' },
+    { label: 'Last Name', name: 'lastName' },
+  ],
+  sponsor: [
+    { label: 'Company', name: 'company' },
+    { label: 'Phone Number', name: 'phoneNumber' },
+  ],
+  alumni: [
+    { label: 'First Name', name: 'firstName' },
+    { label: 'Last Name', name: 'lastName' },
+    { label: 'Company', name: 'company' },
+  ],
+};
+
+const FormComponent: React.FC<FormComponentProps> = ({ fields, onSubmit }) => (
+  <form className={classes.form} onSubmit={onSubmit}>
+    <Stack gap={30}>
+      {fields.map((field) => (
+        <TextInput
+          key={field.name}
+          label={field.label}
+          name={field.name}
+          size="lg"
+          required
+          classNames={{ label: classes.formLabel }}
+        />
+      ))}
+      <TextInput
+        label="Email"
+        name="email"
+        size="lg"
+        required
+        classNames={{ label: classes.formLabel }}
+      />
+      <Flex justify="space-between">
+        <PasswordInput
+          label="Password"
+          name="password"
+          size="lg"
+          required
+          classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
+        />
+        <PasswordInput
+          label="Confirmed Password"
+          name="confirmPassword"
+          size="lg"
+          required
+          classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
+        />
+      </Flex>
+      <Checkbox
+        size="lg"
+        label={
+          <>
+            I accept{' '}
+            <Anchor href="https://mantine.dev" target="_blank" inherit>
+              terms and conditions
+            </Anchor>
+          </>
+        }
+        required
+        name="terms"
+      />
+      <Button color="var(--mantine-color-customAzureBlue-1)" size="lg" type="submit">
+        Confirm
+      </Button>
+    </Stack>
+  </form>
+);
+
 const SignupForm = () => {
-  const [role, setRole] = useState('alumni');
+  const [role, setRole] = useState<string>('alumni');
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
+
+    // Add form submission logic here
+  };
+
   return (
     <div className={classes.signupFormContainer}>
-      {/* logo */}
       <Title order={5} ta="center" mt="md" mb={50}>
         Join the FSAE:47 Job Board Community
       </Title>
 
-      {/* sponsor form */}
-      {role === 'student' && (
-        <form className={classes.form}>
-          <Stack gap={30}>
-            <TextInput
-              label="First Name"
-              size="lg"
-              required
-              classNames={{ label: classes.formLabel }}
-            />
-            <TextInput
-              label="Last Name"
-              size="lg"
-              required
-              classNames={{ label: classes.formLabel }}
-            />
-            <TextInput label="Email" size="lg" required classNames={{ label: classes.formLabel }} />
-            <Flex justify="space-between">
-              <PasswordInput
-                label="Password"
-                size="lg"
-                required
-                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
-              />
-              <PasswordInput
-                label="Confirmed Password"
-                size="lg"
-                required
-                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
-              />
-            </Flex>
-            <Checkbox
-              size="lg"
-              label={
-                <>
-                  I accept{' '}
-                  <Anchor href="https://mantine.dev" target="_blank" inherit>
-                    terms and conditions
-                  </Anchor>
-                </>
-              }
-              required
-            />
-            <Button color="var(--mantine-color-customAzureBlue-1)" size="lg">
-              Confirm
-            </Button>
-          </Stack>
-        </form>
-      )}
-
-      {/* student form */}
-      {role === 'sponsor' && (
-        <form className={classes.form}>
-          <Stack gap={30}>
-            <TextInput
-              label="Company"
-              size="lg"
-              required
-              classNames={{ label: classes.formLabel }}
-            />
-            <TextInput
-              label="Phone Number"
-              size="lg"
-              required
-              classNames={{ label: classes.formLabel }}
-            />
-            <TextInput label="Email" size="lg" required classNames={{ label: classes.formLabel }} />
-            <Flex justify="space-between">
-              <PasswordInput
-                label="Password"
-                size="lg"
-                required
-                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
-              />
-              <PasswordInput
-                label="Confirmed Password"
-                size="lg"
-                required
-                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
-              />
-            </Flex>
-            <Checkbox
-              size="lg"
-              label={
-                <>
-                  I accept{' '}
-                  <Anchor href="https://mantine.dev" target="_blank" inherit>
-                    terms and conditions
-                  </Anchor>
-                </>
-              }
-              required
-            />
-            <Button color="var(--mantine-color-customAzureBlue-1)" size="lg">
-              Confirm
-            </Button>
-          </Stack>
-        </form>
-      )}
-
-      {/* alumni form */}
-      {role === 'alumni' && (
-        <form className={classes.form}>
-          <Stack gap={30}>
-            <TextInput
-              label="First Name"
-              size="lg"
-              required
-              classNames={{ label: classes.formLabel }}
-            />
-            <TextInput
-              label="Last Name"
-              size="lg"
-              required
-              classNames={{ label: classes.formLabel }}
-            />
-            <TextInput
-              label="Company"
-              size="lg"
-              required
-              classNames={{ label: classes.formLabel }}
-            />
-
-            <TextInput label="Email" size="lg" required classNames={{ label: classes.formLabel }} />
-            <Flex justify="space-between">
-              <PasswordInput
-                label="Password"
-                size="lg"
-                required
-                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
-              />
-              <PasswordInput
-                label="Confirmed Password"
-                size="lg"
-                required
-                classNames={{ label: classes.formLabel, root: classes.horizontalInput }}
-              />
-            </Flex>
-            <Checkbox
-              size="lg"
-              label={
-                <>
-                  I accept{' '}
-                  <Anchor href="https://mantine.dev" target="_blank" inherit>
-                    terms and conditions
-                  </Anchor>
-                </>
-              }
-              required
-            />
-            <Button color="var(--mantine-color-customAzureBlue-1)" size="lg">
-              Confirm
-            </Button>
-          </Stack>
-        </form>
-      )}
+      {role && <FormComponent fields={fieldsByRole[role]} role={role} onSubmit={handleSubmit} />}
     </div>
   );
 };
