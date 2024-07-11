881a5782059255708a44280a2fc9cab591608672
Jack Haddad <jhad966@aucklanduni.ac.nz>
2024-07-11 12:13:26 +1200

    restructure

--- web/src/app/pages/Home.page.tsx
+++ web/src/app/pages/Home.page.tsx
@@ -1,27 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Flex, Title, Image, Anchor } from '@mantine/core';
-import logo from '../assets/images/logo.png';
+import {Image} from '@mantine/core'
 
 export function HomePage() {
   return (
     <>
-      {/* Temporary buttons for route testing */}
-      <Flex justify="right" gap="md" mt="md" mr="md">
-        <NavLink to="/signup">
-          <Button variant="filled" color="customPapayaOrange">
-            Sign up
-          </Button>
-        </NavLink>
-        <NavLink to="/login">
-          <Button color="customAzureBlue">Log in</Button>
-        </NavLink>
-      </Flex>
-      <Flex justify="center" gap="md" mt="md" mr="md">
-        <Title order={1}>Home Page</Title>
-      </Flex>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Image src="home_background.jpg" fit="cover" />
+      {/* <Welcome /> */}
+      {/* <ColorSchemeToggle /> */}
     </>
   );
 }
