32a3aeb1f4c8bc9442e6db8dd1c233114942b6db
Jack Haddad <jhad966@aucklanduni.ac.nz>
2024-07-19 11:45:46 +1200

    Fixed homepage code to demo to Kinzi

--- web/src/app/pages/Home.page.tsx
+++ web/src/app/pages/Home.page.tsx
@@ -1,13 +1,15 @@
 import { RootState } from '../../app/store';
 import { useSelector } from 'react-redux';
 import { Image } from '@mantine/core';
-
+import { StudentGuide } from '../components/Home/Guides';
 export function HomePage() {
   const userType = useSelector((state: RootState) => state.user.userType);
 
   return (
     <>
       <Image src="home_background.jpg"/>
+      {userType === 'student' ? <StudentGuide /> : null}
+      
     </>
   );
 }
