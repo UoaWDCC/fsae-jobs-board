9315763c6bf2b42a472bb9e04ba38ed501b08a0b
Kinzi <100537572+Kinzi-c@users.noreply.github.com>
2024-07-20 12:56:18 +1200

    Add FindOutMore component and opacity to Welcome component Background image

---
+++ web/src/app/components/FindOutMore/FindOutMore.tsx
@@ -0,0 +1,5 @@
+import { Flex, Box, Text, Button } from '@mantine/core';
+
+export function FindOutMore() {
+  return <></>;
+}
--- web/src/app/pages/StudentProfile.page.tsx
+++ web/src/app/pages/StudentProfile.page.tsx
@@ -75,16 +75,7 @@ export function StudentProfile() {
       }}
     >
       <Card h={280} className={classes.card}>
-        <Card.Section
-          h={250}
-          style={{
-            backgroundImage:
-              'url(https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80)',
-            cursor: 'pointer',
-          }}
-          className={classes.banner}
-          onClick={handleBannerChange}
-        />
+        <Card.Section h={250} className={classes.banner} onClick={handleBannerChange} />
         {userData?.firstName && (
           <Text className={classes.name} pl={170} pt={130}>
             {userData.firstName} {userData.lastName}
