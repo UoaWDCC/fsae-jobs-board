79bb89b7210e236aa4cbe69f15856fd090405a65
Jolin <jche521@aucklanduni.ac.nz>
2024-07-24 18:36:49 +1200

    most styling done for job list item

--- web/src/app/components/JobBoard/JobListing.tsx
+++ web/src/app/components/JobBoard/JobListing.tsx
@@ -1,20 +1,35 @@
-import { Flex, TextInput, Title } from '@mantine/core';
+import { Flex, Grid, SimpleGrid, Stack, TextInput, Title } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
 import classes from './JobBoard.module.css';
+import JobListingItem from './JobListingItem';
 
 const JobListing = () => {
   return (
-    <Flex
-      justify="center"
-      align="center"
-      gap="md"
-      mt="md"
-      mr="md"
-      className={classes.listingContainer}
-    >
-      <Title order={3}>FIND JOBS</Title>
-      <TextInput placeholder="Search jobs" rightSection={<IconSearch />} />
-    </Flex>
+    <Stack className={classes.listingContainer}>
+      <Flex justify="center" align="center" gap="md" mt="md" mr="md">
+        <Title order={3}>FIND JOBS</Title>
+        <TextInput placeholder="Search jobs" rightSection={<IconSearch />} />
+      </Flex>
+      <SimpleGrid style={{ width: '100%' }} cols={2}>
+        <JobListingItem
+          title={'Junior Software Developer'}
+          company={'WDCC'}
+          location={'Auckland CBD, Auckland'}
+          description={
+            'In this role, you will work to design, develop, and maintain software solutions using .NET, Typescript, and JavaScript.'
+          }
+        />
+
+        <JobListingItem
+          title={'Junior Software Developer'}
+          company={'WDCC'}
+          location={'Auckland CBD, Auckland'}
+          description={
+            'In this role, you will work to design, develop, and maintain software solutions using .NET, Typescript, and JavaScript.'
+          }
+        />
+      </SimpleGrid>
+    </Stack>
   );
 };
 
--- web/src/app/components/JobBoard/JobListingItem.tsx
+++ web/src/app/components/JobBoard/JobListingItem.tsx
@@ -1,5 +1,23 @@
-const JobListingItem = () => {
-  return <>item</>;
+import { FC } from 'react';
+import classes from './JobBoard.module.css';
+
+interface JobListingItemProps {
+  title: string;
+  company: string;
+  location: string;
+  description: string;
+}
+const JobListingItem: FC<JobListingItemProps> = ({ title, company, location, description }) => {
+  return (
+    <div className={classes.listItemContainer}>
+      <p className={classes.listItemTitle}>{title}</p>
+      <div className={classes.listItemCompanyContainer}>
+        <p className={classes.listItemCompanyName}>{company}</p>
+      </div>
+      <p className={classes.listItemLocation}>{location}</p>
+      <p className={classes.listItemDescription}>{description}</p>
+    </div>
+  );
 };
 
 export default JobListingItem;
