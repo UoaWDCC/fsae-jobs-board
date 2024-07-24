8b67ff464b7717bcc24295e46f41b68995a36ad7
Jolin <jche521@aucklanduni.ac.nz>
2024-07-24 17:24:53 +1200

    migrate into components

--- web/src/app/components/JobBoard/JobListing.tsx
+++ web/src/app/components/JobBoard/JobListing.tsx
@@ -1,9 +1,17 @@
 import { Flex, TextInput, Title } from '@mantine/core';
 import { IconSearch } from '@tabler/icons-react';
+import classes from './JobBoard.module.css';
 
 const JobListing = () => {
   return (
-    <Flex justify="center" align="center" gap="md" mt="md" mr="md">
+    <Flex
+      justify="center"
+      align="center"
+      gap="md"
+      mt="md"
+      mr="md"
+      className={classes.listingContainer}
+    >
       <Title order={3}>FIND JOBS</Title>
       <TextInput placeholder="Search jobs" rightSection={<IconSearch />} />
     </Flex>
--- web/src/app/pages/JobBoard.page.tsx
+++ web/src/app/pages/JobBoard.page.tsx
@@ -4,11 +4,18 @@ import JobListing from '../components/JobBoard/JobListing';
 
 export function JobBoard() {
   return (
-    <div style={{ paddingTop: '3rem' }}>
-      <Group>
-        <JobFilter />
-        <JobListing />
-      </Group>
+    <div
+      style={{
+        paddingTop: '3rem',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}
+    >
+      <JobFilter />
+      <JobListing />
     </div>
   );
 }
