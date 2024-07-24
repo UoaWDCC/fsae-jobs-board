284fd30f30cfabf44a6ff8bf8ed0fab97940bb65
Jolin <jche521@aucklanduni.ac.nz>
2024-07-24 17:25:02 +1200

    skeleton of filter section

--- web/src/app/components/JobBoard/JobFilter.tsx
+++ web/src/app/components/JobBoard/JobFilter.tsx
@@ -1,12 +1,33 @@
-import { Stack } from '@mantine/core';
-
+import { Checkbox, Stack } from '@mantine/core';
+import classes from './JobBoard.module.css';
+import { useState } from 'react';
 const JobFilter = () => {
+  const [roles, setRoles] = useState<string[]>([]);
+  const [fields, setFields] = useState<string[]>([]);
+
   return (
-    <>
+    <Stack className={classes.filterContainer}>
+      <p>Filters</p>
       <Stack>
-        <p>filter section here</p>
+        <p>Role Type</p>
+        <Checkbox.Group value={roles} onChange={setRoles}>
+          <Checkbox value="internship" label="Internships" />
+          <Checkbox value="graduate" label="Graduate Roles" />
+          <Checkbox value="junior" label="Junior Roles" />
+        </Checkbox.Group>
       </Stack>
-    </>
+      <Stack>
+        <p>Fields</p>
+        <Checkbox.Group value={fields} onChange={setFields}>
+          <Checkbox value="auto" label="Automation" />
+          <Checkbox value="electrical" label="Electrical" />
+          <Checkbox value="mechanical" label="Mechanical" />
+          <Checkbox value="mechatronics" label="Mechatronics" />
+          <Checkbox value="software" label="Software" />
+          <Checkbox value="other" label="Other" />
+        </Checkbox.Group>
+      </Stack>
+    </Stack>
   );
 };
 
