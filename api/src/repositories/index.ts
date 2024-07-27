6af59573d0a070881cdbb3b704a68ce23df991f9
Don <donsqlin@gmail.com>
2024-07-27 22:24:24 +1200

    Revert "create FsaeUser repository"
    This reverts commit 8d212291cf488edd1a05f14a1d91640df755c50d.

--- api/src/repositories/index.ts
+++ api/src/repositories/index.ts
@@ -2,4 +2,3 @@ export * from './admin.repository';
 export * from './alumni.repository';
 export * from './member.repository';
 export * from './sponsor.repository';
-export * from './fsae-user.repository';
