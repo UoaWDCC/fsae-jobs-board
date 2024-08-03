a22edef600b204db3f7929f2cbe3e186d28edaf3
Don <donsqlin@gmail.com>
2024-08-03 18:24:25 +1200

    Revert "create alumni controller"
    This reverts commit a7a9e7f6d7e8710724652c026bf6027286347190.

--- api/src/controllers/index.ts
+++ api/src/controllers/index.ts
@@ -2,4 +2,3 @@ export * from './ping.controller';
 export * from './login.controller';
 export * from './register.controller';
 export * from './sponsor.controller';
-export * from './alumni.controller';
