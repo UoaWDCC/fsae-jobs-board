3cb5d70d02677a8064679d404c6ed9ddc2b47527
Yang Qian <30404287+GodYazza@users.noreply.github.com>
2024-08-07 14:33:55 +1200

    Revert "Feat/integrate login"

--- web/src/app/components/AuthForms/LoginForm.tsx
+++ web/src/app/components/AuthForms/LoginForm.tsx
@@ -3,41 +3,29 @@ import classes from './authform.module.css';
 import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { setUserType } from '../../features/user/userSlice';
 import { toast } from 'react-toastify';
-import {login} from "@/api/login";
-import {useState} from "react";
 
 export function LoginForm() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  // const handleLoginAs = (userType: 'student' | 'sponsor' | 'alumni' | 'admin') => {
-  //
-  //
-  //    // dispatch(setUserType(userType)); Todo: Move this to API folder
-  //   // const profilePath = {
-  //   //   student: '/profile/student',
-  //   //   sponsor: '/profile/sponsor',
-  //   //   alumni: '/profile/alumni',
-  //   //   admin: '/profile/admin',
-  //   // }[userType];
-  //   // toast.success('Logged in as ' + userType);
-  //   // navigate(profilePath, { replace: true });
-  // };
-
-  async function onLogin() {
-    await login(email, password)
-      .then((response) => {
-        toast.success('Login Successful');
-        // Todo: Do something afterwards.
-        // handleLoginAs(response.data.userType);
-      }).catch((error) => {
-        console.log(error)
-        toast.error(error.toString());
-      })
-  }
+  const handleLoginAs = (userType: 'student' | 'sponsor' | 'alumni' | 'admin') => {
+    // Simulate successful login (to be replaced with the actual authentication logic)
+    const mockToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+    localStorage.setItem('accessToken', mockToken);
+    // If authentication is successful: Update Redux store with userType
+    dispatch(setUserType(userType));
+    // Redirect to the appropriate profile page based on userType
+    const profilePath = {
+      student: '/profile/student',
+      sponsor: '/profile/sponsor',
+      alumni: '/profile/alumni',
+      admin: '/profile/admin',
+    }[userType];
+    toast.success('Logged in as ' + userType);
+    navigate(profilePath, { replace: true });
+  };
 
   return (
     <Flex
@@ -52,12 +40,26 @@ export function LoginForm() {
           Login
         </Title>
 
-        <TextInput placeholder="Enter email" size="lg" mb="lg" value={email} onChange={(event) => setEmail(event.target.value)}/>
-        <PasswordInput placeholder="Enter password" mt="xl" size="lg" value={password} onChange={(event) => setPassword(event.target.value)}/>
+        <TextInput placeholder="Enter email" size="lg" mb="lg" />
+        <PasswordInput placeholder="Enter password" mt="xl" size="lg" />
         <Checkbox label="Remember Me" mt="xl" size="md" />
-        <Button fullWidth mt="xl" mb="md" size="lg" onClick={onLogin}>
+        <Button fullWidth mt="xl" mb="md" size="lg">
           Login
         </Button>
+        <Flex justify="center" gap="md" mt="md" mr="md">
+          <Button variant="filled" color="green" onClick={() => handleLoginAs('student')}>
+            Student
+          </Button>
+          <Button variant="filled" color="blue" onClick={() => handleLoginAs('sponsor')}>
+            Sponsor
+          </Button>
+          <Button variant="filled" color="violet" onClick={() => handleLoginAs('alumni')}>
+            Alumni
+          </Button>
+          <Button variant="filled" color="red" onClick={() => handleLoginAs('admin')}>
+            Admin
+          </Button>
+        </Flex>
         <Text ta="center" mt="xl">
           Don&apos;t have an account?{' '}
           <NavLink to="/" className={classes.link}>
