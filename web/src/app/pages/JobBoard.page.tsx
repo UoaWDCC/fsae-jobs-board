2d193bccfea8363c78c457e8e9bbe54c3ecf73e8
Jolin <jche521@aucklanduni.ac.nz>
2024-08-09 23:08:17 +1200

    add some responsiveness to the job components (pagination is not good tho)

--- web/src/app/pages/JobBoard.page.tsx
+++ web/src/app/pages/JobBoard.page.tsx
@@ -2,23 +2,42 @@ import { Divider } from '@mantine/core';
 import JobFilter from '../components/JobBoard/JobFilter';
 import JobListing from '../components/JobBoard/JobListing';
 import classes from '../styles/JobBoardPage.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function JobBoard() {
   const [filterRoles, setFilterRoles] = useState<string[]>([]);
   const [filterFields, setFilterFields] = useState<string[]>([]);
 
+  const [isPortrait, setIsPortrait] = useState(window.innerHeight > window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsPortrait(window.innerHeight > window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className={classes.jobBoardContainer}>
-      <div className={classes.leftContainer}>
-        <JobFilter
-          filterRoles={filterRoles}
-          setFilterRoles={setFilterRoles}
-          filterFields={filterFields}
-          setFilterFields={setFilterFields}
-        />
-      </div>
-      <Divider orientation="vertical" />
+      {isPortrait ? (
+        <></>
+      ) : (
+        <div className={classes.leftContainer}>
+          <JobFilter
+            filterRoles={filterRoles}
+            setFilterRoles={setFilterRoles}
+            filterFields={filterFields}
+            setFilterFields={setFilterFields}
+          />
+        </div>
+      )}
+
+      {!isPortrait && <Divider orientation="vertical" />}
 
       <JobListing filterRoles={filterRoles} filterFields={filterFields} />
     </div>
