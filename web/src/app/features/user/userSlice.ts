9e46b133c5b9d51c8e73d9a6d3579dafeb43db6a
Yang Qian <30404287+GodYazza@users.noreply.github.com>
2024-09-08 22:11:12 +1200

    Reapply "Feat/integrate login"
    This reverts commit 3cb5d70d02677a8064679d404c6ed9ddc2b47527.

--- web/src/app/features/user/userSlice.ts
+++ web/src/app/features/user/userSlice.ts
@@ -1,25 +1,38 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import {Role} from "@/app/type/role";
+
+// Todo: Remove this duplicate type definition...
 export type UserType = 'student' | 'sponsor' | 'alumni' | 'admin';
 
 // Define the user state interface
-interface UserState {
-  userType: UserType | null; // Allow for null (unauthenticated)
+export interface UserState {
+  id: string;
+  email: string;
+  username: string;
+  activated: boolean,
+  firstName: string,
+  lastName: string,
+  phoneNumber: string,
+  desc?: string
+  UserType?: Role // Allow for null (unauthenticated)
   // ... other user data (name, email, etc.) as needed
 }
 
 // Initial state
 const initialState: UserState = {
-  userType: null,
+  id: '',
+  email: '',
+  username: '',
+  activated: false,
+  firstName: '',
+  lastName: '',
+  phoneNumber: '',
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-    setUserType: (state, action: PayloadAction<UserType>) => {
-      // Type-safe payload
-      state.userType = action.payload;
-    },
     resetUser() {
       return initialState;
     },
--- web/src/app/type/role.ts
+++ web/src/app/type/role.ts
@@ -2,4 +2,12 @@ export enum Role {
   Student = 'student',
   Sponsor = 'sponsor',
   Alumni = 'alumni',
+  Admin = 'admin',
+  Member = 'member',
+  Unknown = 'Unknown' // Fallback in case error
+}
+
+export function stringToRole(roleString) {
+  const roleKey = Object.keys(Role).find(key => Role[key] === roleString);
+  return roleKey ? Role[roleKey] : Role.Unknown;
 }
