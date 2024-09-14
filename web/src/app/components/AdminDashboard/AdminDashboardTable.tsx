4b455ffa69c10f636c8492c49bf9e868c0e092a0
Jolin <jche521@aucklanduni.ac.nz>
2024-09-14 15:12:50 +1200

    Revert "migrate to use datatable"
    This reverts commit 849b69bdf028e6fcf2ac92fcb20e6014dfb6dfdf.

--- web/src/app/components/AdminDashboard/AdminDashboardTable.tsx
+++ web/src/app/components/AdminDashboard/AdminDashboardTable.tsx
@@ -1,39 +1,13 @@
 import { Stack, Table, Text } from '@mantine/core';
-import 'mantine-datatable/styles.css';
 import styles from './AdminDashboard.module.css';
 import { AdminReview } from '@/app/models/adminReview';
 import { FC } from 'react';
 import { date2string } from '@/app/features/date/dateConverter';
-import { DataTable } from 'mantine-datatable';
 
 interface Props {
   data: AdminReview[];
 }
 const AdminDashboardTable: FC<Props> = ({ data }) => {
-  // id: '1',
-  // name: 'Google',
-  // userType: Role.Alumni,
-  // date: new Date(),
-  // status: Status.Approved,
-  const columns = [
-    {
-      accessor: 'name',
-      cellsClassName: styles.leftRoundedCell,
-    },
-    {
-      accessor: 'userType',
-      cellsClassName: styles.tableRow,
-    },
-    {
-      accessor: 'date',
-      render: ({ date }) => date2string(date),
-      cellsClassName: styles.tableRow,
-    },
-    {
-      accessor: 'status',
-      cellsClassName: styles.rightRoundedCell,
-    },
-  ];
   return (
     <Stack justify="center" align="center" gap="md" mt="md">
       <div className={styles.tableContainer}>
@@ -59,17 +33,6 @@ const AdminDashboardTable: FC<Props> = ({ data }) => {
             </Table.Tbody>
           </Table>
         </Table.ScrollContainer>
-        {data && (
-          <DataTable
-            records={data}
-            columns={columns}
-            backgroundColor="transparent"
-            rowBorderColor="black"
-            verticalSpacing="lg"
-            c="white"
-            rowClassName={styles.tableRow}
-          />
-        )}
       </div>
     </Stack>
   );
