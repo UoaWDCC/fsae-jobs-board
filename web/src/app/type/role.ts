5e4502f444c399e859cc3967ac720f6fe407ba1b
Jolin Chen <jche521@aucklanduni.ac.nz>
2024-09-15 13:38:38 +1200

    remove yarn lock (no need since we are using npm) abd duplicate function

--- web/src/app/type/role.ts
+++ web/src/app/type/role.ts
@@ -4,11 +4,11 @@ export enum Role {
   Alumni = 'alumni',
   Admin = 'admin',
   Member = 'member',
-  Unknown = 'Unknown' // Fallback in case error
+  Unknown = 'Unknown', // Fallback in case error
 }
 
 export function stringToRole(roleString) {
-  const roleKey = Object.keys(Role).find(key => Role[key] === roleString);
+  const roleKey = Object.keys(Role).find((key) => Role[key] === roleString);
   return roleKey ? Role[roleKey] : Role.Unknown;
 }
 
@@ -16,9 +16,9 @@ export function roleToString(role: Role): string {
   return role;
 }
 
-export function stringToRole(value: string): Role | undefined {
-  return Object.values(Role).includes(value as Role) ? (value as Role) : undefined;
-}
+// export function stringToRole(value: string): Role | undefined {
+//   return Object.values(Role).includes(value as Role) ? (value as Role) : undefined;
+// }
 
 export function stringsToRoles(values: string[]): Role[] {
   return values
