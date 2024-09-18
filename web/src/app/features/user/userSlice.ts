974a22307d1827cf07b8265ed599ac8d6dc20a95
Yang Qian <30404287+GodYazza@users.noreply.github.com>
2024-09-18 21:54:39 +1200

    fix userSlice

--- web/src/app/features/user/userSlice.ts
+++ web/src/app/features/user/userSlice.ts
@@ -1,20 +1,17 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import {Role} from "@/app/type/role";
-
-// Todo: Remove this duplicate type definition...
-export type UserType = 'student' | 'sponsor' | 'alumni' | 'admin';
+import { Role } from "@/app/type/role";
 
 // Define the user state interface
 export interface UserState {
   id: string;
   email: string;
   username: string;
-  activated: boolean,
-  firstName: string,
-  lastName: string,
-  phoneNumber: string,
-  desc?: string
-  UserType?: Role // Allow for null (unauthenticated)
+  activated: boolean;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  desc?: string;
+  userType?: Role; // Allow for null (unauthenticated)
   // ... other user data (name, email, etc.) as needed
 }
 
@@ -29,6 +26,7 @@ const initialState: UserState = {
   phoneNumber: '',
 };
 
+// Create a slice with reducers
 const userSlice = createSlice({
   name: 'user',
   initialState,
@@ -36,9 +34,13 @@ const userSlice = createSlice({
     resetUser() {
       return initialState;
     },
+    setUserType(state, action: PayloadAction<Role | undefined>) {
+      state.userType = action.payload;
+    },
     // ... other reducers for user actions (e.g., login, logout, update profile)
   },
 });
 
+// Export the actions and the reducer
 export const { setUserType, resetUser } = userSlice.actions;
 export default userSlice.reducer;
