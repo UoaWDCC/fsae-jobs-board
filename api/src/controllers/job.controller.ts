80474b4ae029902a25421b78037c0880a6491fb1
Carl Gabriel <carldelapena2004@gmail.com>
2025-07-11 22:55:53 +1200

    Restored authentication to match main branch to prepare for PR
    I initially turned off authentication from jobcontroller in my device for easier debugging.
    
    I did not intend to fully remove authentication, just for debug purposes. Restored now for PR

--- api/src/controllers/job.controller.ts
+++ api/src/controllers/job.controller.ts
@@ -1,21 +1,19 @@
-//TEMPORARY: Temporarily remove AUTH protection for testing job ads (Will re-enable and uncomment authentication after search functionality is added)
-
 import {Filter, FilterExcludingWhere, repository} from '@loopback/repository';
 import {post, param, get, getModelSchemaRef, patch, del, requestBody, response} from '@loopback/rest';
 import {inject} from '@loopback/core';
 import {JobAd} from '../models';
 import {JobAdRepository} from '../repositories';
 import {UserProfile} from '@loopback/security';
-//import {authenticate, AuthenticationBindings} from '@loopback/authentication';
+import {authenticate, AuthenticationBindings} from '@loopback/authentication';
 import {authorize} from '@loopback/authorization';
 import {HttpErrors} from '@loopback/rest';
 import {FsaeRole} from '../models';
 
-// @authenticate('fsae-jwt')
+@authenticate('fsae-jwt')
 export class JobController {
   constructor(
     @repository(JobAdRepository) public jobAdRepository : JobAdRepository,
-    // @inject(AuthenticationBindings.CURRENT_USER) private currentUserProfile: UserProfile,
+    @inject(AuthenticationBindings.CURRENT_USER) private currentUserProfile: UserProfile,
   ) {}
 
   // TEMPORARY: Remove protection for testing job ads
@@ -41,7 +39,7 @@ export class JobController {
     jobAdData: Omit<JobAd, 'id' | 'publisherID'>,
   ): Promise<JobAd> {
     const jobAd = new JobAd(jobAdData);
-    // jobAd.publisherID = this.currentUserProfile.id.toString();
+    jobAd.publisherID = this.currentUserProfile.id.toString();
     return this.jobAdRepository.create(jobAd);
   }
 
@@ -62,18 +60,8 @@ export class JobController {
     },
   })
   async find(
-    @param.query.string('search') search?: string,
+    @param.filter(JobAd) filter?: Filter<JobAd>,
   ): Promise<JobAd[]> {
-    const filter = search
-      ? {
-          where: {
-            or: [
-              {title: {like: search, options: 'i'}},
-              {description: {like: search, options: 'i'}},
-            ],
-          },
-        }
-      : {};
     return this.jobAdRepository.find(filter);
   }
 
