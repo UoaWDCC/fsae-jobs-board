4983446a43a9f04e12185f16b3278cde7cee7afd
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-08-06 22:15:41 +1200

    Add firstName and lastName fields to admin model

--- api/src/models/admin.model.ts
+++ api/src/models/admin.model.ts
@@ -3,7 +3,17 @@ import {FsaeUser} from './fsae-user.model';
 
 @model()
 export class Admin extends FsaeUser {
+  @property({
+    type: 'string', 
+    required: true
+  })
+  firstName: string;
 
+  @property({
+    type: 'string', 
+    required: true
+  })
+  lastName: string;
   
   constructor(data?: Partial<Admin>) {
     super(data);
