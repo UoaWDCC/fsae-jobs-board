91d4465207960847b16f61ee0dd8a242f2660d52
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-08-08 19:49:09 +1200

    test(e2e): added test for saving first name locally

--- web/cypress/e2e/03_member_profile_edit.cy.ts
+++ web/cypress/e2e/03_member_profile_edit.cy.ts
@@ -3,10 +3,23 @@ beforeEach(() => {
   cy.get('input[placeholder="Enter email"]').type('validmember@gmail.com');
   cy.get('input[placeholder="Enter password"]').type('ValidPassword123!');
   cy.contains('button', 'Login').click();
-
-  // successful login is directed to member profile page
-  cy.url().should('include', '/profile/member');
+  cy.url().should('include', '/profile/member'); // successful login is directed to member profile page
+  cy.contains('span', 'Edit Profile').click();
 });
 
+describe('Tests successful edits to about me section in member profile', () => {
+  it('should save first name locally', () => {
+    cy.get('input[name="firstName"]').clear().type('newFirstName');
+    cy.get('button[name="profileEditSave"]').click({ force: true }); // force click to bypass toast message
+    Cypress.on('uncaught:exception', (err, runnable) => {
+      return false; // prevent cypress from failing the test due to unimplemented endpoints
+    });
 
-
+    cy.get('[data-test="fullName"]')
+      .invoke('text')
+      .then((fullName) => {
+        const firstName = fullName.trim().split(' ')[0]; // split by space
+        expect(firstName).to.eq('newFirstName');
+      });
+  });
+});
