f2e286473258ee732b0eda063236fc5643d2e900
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-08-16 17:48:46 +1200

    Extract field display map objects into their own file

--- web/src/app/pages/Student/StudentProfile.page.tsx
+++ web/src/app/pages/Student/StudentProfile.page.tsx
@@ -25,26 +25,10 @@ import { jwtDecode } from 'jwt-decode';
 import DeactivateAccountModal from '../../components/Modal/DeactivateAccountModal';
 import { JobType } from '@/models/job-type';
 import { SubGroup } from '@/models/subgroup.model';
+import { jobTypeDisplayMap, subGroupDisplayMap } from '@/app/utils/field-display-maps';
         
-const PLACEHOLDER_BANNER = "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80"
-const PLACEHOLDER_AVATAR = "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-9.png"
 
-const jobTypeDisplayMap: Record<JobType, string> = {
-  [JobType.GRAD_ROLE]: "Graduate Role",
-  [JobType.INTERNSHIP]: "Internship",
-  [JobType.PART_TIME]: "Part-time work",
-  [JobType.NOT_FOR_HIRE]: "None"
-}
 
-const subGroupDisplayMap: Record<SubGroup, string> = {
-  [SubGroup.UNKNOWN]: "Team Member",
-  [SubGroup.BUSINESS]: "Business Team",
-  [SubGroup.COMPOSITES]: "Composites Team",
-  [SubGroup.MECHANICAL]: "Mechanical Team",
-  [SubGroup.ELECTRICAL]: "Electrical Team",
-  [SubGroup.AUTONOMOUS]: "Autonomous Team",
-  [SubGroup.RACE_TEAM]: "Race Team",
-}
 
 export function StudentProfile() {
   // UseState for future modal implementation
