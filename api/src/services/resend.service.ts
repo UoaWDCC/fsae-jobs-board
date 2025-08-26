7db8870579961673093f26be7c0cb283ecc9cd30
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-08-26 12:19:25 +1200

    add: unique time identifier for html template + small refactor for resend service
    emails that are sent too quick in succession with similar content might have its' content blanked out by resend, added a unique time identifier to the html for that reason.

--- api/src/services/resend.service.ts
+++ api/src/services/resend.service.ts
@@ -6,10 +6,16 @@ import {BindingScope, injectable} from '@loopback/core';
 @injectable({scope: BindingScope.TRANSIENT})
 export class ResendService {
   private client: Resend;
+  private htmlTemplate: string;
 
   constructor() {
     const apiKey = process.env.RESEND_API_KEY || '';
     this.client = new Resend(apiKey);
+    const templatePath = path.join(
+      __dirname,
+      '../email-templates/email-verification.html',
+    );
+    this.htmlTemplate = fs.readFileSync(templatePath, 'utf8');
   }
 
   async sendVerificationEmail(
@@ -17,22 +23,17 @@ export class ResendService {
     firstName: string,
     verificationCode: string,
   ) {
-    const templatePath = path.join(
-      __dirname,
-      '../email-templates/email-verification.html',
-    );
-    let templateHtml = fs.readFileSync(templatePath, 'utf8');
-
-    templateHtml = templateHtml
+    const html = this.htmlTemplate
       .replace('{{first_name}}', firstName)
-      .replace('{{verification_code}}', verificationCode);
+      .replace('{{verification_code}}', verificationCode)
+      .replace('{{timestamp}}', Date.now().toString());
 
     const verification = await this.client.emails
       .send({
         from: 'delivered@resend.dev',
         to: [email],
         subject: 'Verify Your Email Address',
-        html: templateHtml,
+        html,
       })
       .catch(error => {
         console.error('Resend API Error Details:', error.message, error);
