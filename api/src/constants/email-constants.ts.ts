2d82a2384b24eaabbc7e92784cbe82f9f1ff3087
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-08-28 15:54:19 +1200

    chore: move email template to public folder

--- api/src/constants/email-constants.ts.ts
+++ api/src/constants/email-constants.ts.ts
@@ -2,7 +2,7 @@ import path from 'path';
 
 export const EMAIL_VERIFICATION_TEMPLATE_PATH = path.join(
   __dirname,
-  '../email-templates/email-verification.html'
+  '../../public/email-verification.html'
 );
 
 export const SENDER_EMAIL = 'delivered@resend.dev'; // TODO: replace with verified domain later
