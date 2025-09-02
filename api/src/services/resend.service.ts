b363a66c55c4bbc845e8f6ca65ac4ffd280e2272
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-09-02 21:58:49 +1200

    add: password reset function to resend service

--- api/src/services/resend.service.ts
+++ api/src/services/resend.service.ts
@@ -3,19 +3,25 @@ import {Resend} from 'resend';
 import {BindingScope, injectable} from '@loopback/core';
 import {
   EMAIL_VERIFICATION_TEMPLATE_PATH,
+  PASSWORD_RESET_TEMPLATE_PATH,
   SENDER_EMAIL,
 } from '../constants/email-constants.ts';
 
 @injectable({scope: BindingScope.TRANSIENT})
 export class ResendService {
   private client: Resend;
-  private htmlTemplate: string;
+  private emailVerificationTemplate: string;
+  private passwordResetTemplate: string;
 
   constructor() {
     const apiKey = process.env.RESEND_API_KEY || '';
     this.client = new Resend(apiKey);
-    this.htmlTemplate = fs.readFileSync(
+    this.emailVerificationTemplate = fs.readFileSync(
       EMAIL_VERIFICATION_TEMPLATE_PATH,
+      'utf8',
+    );
+    this.passwordResetTemplate = fs.readFileSync(
+      PASSWORD_RESET_TEMPLATE_PATH,
       'utf8',
     );
   }
@@ -30,11 +36,11 @@ export class ResendService {
         throw new Error('Resend client not initialized');
       }
 
-      if (!this.htmlTemplate) {
+      if (!this.emailVerificationTemplate) {
         throw new Error('Email template not loaded');
       }
 
-      const html = this.htmlTemplate
+      const html = this.emailVerificationTemplate
         .replace('{{first_name}}', firstName)
         .replace('{{verification_code}}', verificationCode)
         .replace('{{timestamp}}', Date.now().toString());
@@ -57,6 +63,47 @@ export class ResendService {
       return verification;
     } catch (error) {
       console.error('Error sending verification email:', error);
+      throw error;
+    }
+  }
+
+  async sendPasswordResetEmail(
+    email: string,
+    firstName: string,
+    resetLink: string,
+  ) {
+    try {
+      if (!this.client) {
+        throw new Error('Resend client not initialized');
+      }
+
+      if (!this.passwordResetTemplate) {
+        throw new Error('Email template not loaded');
+      }
+
+      const html = this.passwordResetTemplate
+        .replace('{{first_name}}', firstName)
+        .replace('{{reset_link}}', resetLink)
+        .replace('{{timestamp}}', Date.now().toString());
+
+      const passwordReset = await this.client.emails.send({
+        from: SENDER_EMAIL,
+        to: [email],
+        subject: 'Reset Your Password',
+        html,
+      });
+
+      if (!passwordReset.error) {
+        console.log('Password reset email sent:', passwordReset['data']);
+      } else {
+        console.error(
+          'Error sending password reset email:',
+          passwordReset['error'],
+        );
+      }
+      return passwordReset;
+    } catch (error) {
+      console.error('Error sending password reset email:', error);
       throw error;
     }
   }
