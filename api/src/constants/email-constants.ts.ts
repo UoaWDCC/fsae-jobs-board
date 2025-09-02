8769b10abc7877143cc9ff767de72e22b62f6f38
treyson tsen <78147374+p0nyo@users.noreply.github.com>
2025-09-02 21:59:38 +1200

    add: password email link to email constants file

--- api/src/constants/email-constants.ts.ts
+++ api/src/constants/email-constants.ts.ts
@@ -5,4 +5,11 @@ export const EMAIL_VERIFICATION_TEMPLATE_PATH = path.join(
   '../../public/email-verification.html'
 );
 
+export const PASSWORD_RESET_TEMPLATE_PATH = path.join(
+  __dirname,
+  '../../public/password-reset.html'
+);
+
 export const SENDER_EMAIL = 'delivered@resend.dev'; // TODO: replace with verified domain later
+
+export const PASSWORD_RESET_LINK = 'http://localhost:5173/reset-password/' // TODO: replace with verified domain later
