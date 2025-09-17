2c65637bef192a583f072c8357883709bce9417c
Weizhou Xue <benjamin_xue@outlook.com>
2025-09-17 22:42:14 +1200

    -update necessary packages

--- web/.storybook/preview.tsx
+++ web/.storybook/preview.tsx
@@ -1,25 +1,22 @@
 import '@mantine/core/styles.css';
-import React, { useEffect } from 'react';
-import { addons } from '@storybook/preview-api';
-import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
-import { MantineProvider, useMantineColorScheme } from '@mantine/core';
+import React from 'react';
+import { MantineProvider } from '@mantine/core';
 import { theme } from '../src/app/theme';
 
-const channel = addons.getChannel();
-
-function ColorSchemeWrapper({ children }: { children: React.ReactNode }) {
-  const { setColorScheme } = useMantineColorScheme();
-  const handleColorScheme = (value: boolean) => setColorScheme(value ? 'dark' : 'light');
-
-  useEffect(() => {
-    channel.on(DARK_MODE_EVENT_NAME, handleColorScheme);
-    return () => channel.off(DARK_MODE_EVENT_NAME, handleColorScheme);
-  }, [channel]);
-
-  return <>{children}</>;
-}
-
 export const decorators = [
-  (renderStory: any) => <ColorSchemeWrapper>{renderStory()}</ColorSchemeWrapper>,
-  (renderStory: any) => <MantineProvider theme={theme}>{renderStory()}</MantineProvider>,
+  (Story: any) => (
+    <MantineProvider theme={theme}>
+      <Story />
+    </MantineProvider>
+  ),
 ];
+
+export const parameters = {
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: 'light', color: '#ffffff' },
+      { name: 'dark', class: 'dark', color: '#1a1b1e' }
+    ],
+  },
+};
