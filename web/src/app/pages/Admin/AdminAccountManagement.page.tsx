d9aeb33c901b46a740b1cecbc281eda1f3f477dd
kylerosser <39887097+kylerosser@users.noreply.github.com>
2025-10-02 11:22:25 +1300

    Create a tab in the admin dashboard for admin account management

--- web/src/app/pages/Admin/AdminAccountManagement.page.tsx
+++ web/src/app/pages/Admin/AdminAccountManagement.page.tsx
@@ -1,4 +1,4 @@
 
 export function AdminAccountManagement() {
-    return
+    return <p>test</p>;
 }
--- web/src/app/pages/Admin/AdminDashboard.page.tsx
+++ web/src/app/pages/Admin/AdminDashboard.page.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Tabs } from '@mantine/core';
 import { AdminApplications } from "./AdminApplications.page";
 import { AdminAuditLog } from "./AdminAuditLog.page";
+import { AdminAccountManagement } from "./AdminAccountManagement.page";
 
 export function AdminDashboard() {
     const [activeTab, setActiveTab] = useState<string | null>('requests');
@@ -15,6 +16,9 @@ export function AdminDashboard() {
                 <Tabs.Tab value="audit">
                     Audit Log
                 </Tabs.Tab>
+                <Tabs.Tab value="account">
+                    Admin Accounts
+                </Tabs.Tab>
             </Tabs.List>
 
             <Tabs.Panel value="requests">
@@ -23,6 +27,10 @@ export function AdminDashboard() {
 
             <Tabs.Panel value="audit">
                 <AdminAuditLog />
+            </Tabs.Panel>
+
+            <Tabs.Panel value="account">
+                <AdminAccountManagement />
             </Tabs.Panel>
         </Tabs>
     );
